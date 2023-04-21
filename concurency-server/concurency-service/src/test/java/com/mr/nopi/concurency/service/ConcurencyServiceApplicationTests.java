13f25a46851fcd38a93e841c13c9125e70b1ae41
LGamaliy <lub.gamaliy@gmail.com>
2023-04-21 10:22:55 +0300

    init commit - Initiated initial project structure

---
+++ concurency-server/concurency-service/src/test/java/com/mr/nopi/concurency/service/ConcurencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.nopi.concurency.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurencyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
