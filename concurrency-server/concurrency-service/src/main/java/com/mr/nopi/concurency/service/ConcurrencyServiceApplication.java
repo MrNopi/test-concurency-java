1b0c31d82cbb2153bd51e374c54bb97c94d7ab5d
LGamaliy <lub.gamaliy@gmail.com>
2023-04-21 12:52:42 +0300

    - renaming

--- concurrency-server/concurrency-service/src/main/java/com/mr/nopi/concurency/service/ConcurrencyServiceApplication.java
+++ concurrency-server/concurrency-service/src/main/java/com/mr/nopi/concurency/service/ConcurrencyServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConcurencyServiceApplication {
+public class ConcurrencyServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConcurencyServiceApplication.class, args);
+        SpringApplication.run(ConcurrencyServiceApplication.class, args);
     }
 
 }
